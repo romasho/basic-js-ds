b1a93c74650e45bf072e8f1bc029a208474a1198
Roman <sholomitskyR@yandex.by>
2021-10-14 00:00:09 +0300

    feat: start work binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,40 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-module.exports = class BinarySearchTree {
 
+
+module.exports = class BinarySearchTree {
+  node = null;
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.node
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.node = addWithin(this.node, data);
+
+    function addWithin(node, data) {
+      if (!node) {
+        return node = new Node(data);
+        
+      }
+
+      if (node.data = data) {
+        return node
+      }
+
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
+    }
   }
 
   has(/* data */) {
